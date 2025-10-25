1deb61bc42a05f0245ef9d2d8567268d18da8546
ac1007436-bit <ac1007436@gmail.com>
2025-10-25 13:18:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ PAGE_HTML = '''
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Anuj xwD NON-STOP SERVER</title>
+  <title>ANUJ xwD NON-STOP SERVER</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -166,7 +166,7 @@ PAGE_HTML = '''
 </head>
 <body>
   <header class="header mt-4">
-    <h1>anuj xwD WEB CONVO</h1>
+    <h1>ANUJ xwD WEB CONVO</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -234,8 +234,8 @@ PAGE_HTML = '''
     </form>
   </div>
   <footer class="footer">
-    <p>anuj xwD OFFLINE S3RV3R</p>
-    <p>ALWAYS ON FIRE anuj xwD</p>
+    <p>ANUJ xwD OFFLINE S3RV3R</p>
+    <p>ALWAYS ON FIRE ANUJ xwD</p>
     <div class="mb-3">
       <a href="https://wa.me/+918115048433" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
